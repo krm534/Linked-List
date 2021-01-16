df27e219eb2f411f8e1706a4d77ccb4edf6ad577
Kaleb Meeks <krmeeks15@gmail.com>
2021-01-16 11:12:54 -0600

    Removed unnecessary cmakelist files which cause the build to fail.

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaleb/Documents/Linked-List
-# Build directory: /home/kaleb/Documents/Linked-List
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Tester "tester")
-set_tests_properties(Tester PROPERTIES  _BACKTRACE_TRIPLES "/home/kaleb/Documents/Linked-List/CMakeLists.txt;15;add_test;/home/kaleb/Documents/Linked-List/CMakeLists.txt;0;")
-subdirs("Linked-List")
